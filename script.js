ed1362bdf15c11f80150c1b764d6590d5d738ff7
Krishna-Ramchandani <krishnaramchandani@benchmarkit.solutions>
2021-08-19 00:29:28 +0530

    Variables, values,data types, dynamic typing

---
+++ script.js
@@ -0,0 +1,37 @@
+let js = 'awesome';
+if (js === 'awesome') alert('Inside IF');
+
+console.log(40 + 60);
+
+let firstName = "Krishna";
+console.log(firstName);
+var lastName = "Ramchandani";
+console.log(lastName);
+
+const countryName = "India";
+console.log(countryName);
+let continentName = "Asia";
+console.log(continentName);
+let totalPopulation = 10000
+console.log(totalPopulation);
+
+let age = 18; let bill = 99.99; //number variables;
+let subjectName = "JavaScript"; let subName = 'JavaScript'; //String variables
+let real = true; let unreal = false;//booleans variables;
+let carName, modelNumber;//undefined variables;
+let last = null;//null variable;
+
+console.log(real);
+console.log(typeof (carName));
+age = 'Eighteen'; //dynamic typing converting number to string
+console.log(typeof (age));
+
+console.log(carName);
+console.log(typeof (carName));
+
+carName = 'Mercedes';
+console.log(carName);
+console.log(typeof (carName));
+
+
+
