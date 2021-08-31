9cecc8e2c7c294f020b5486d0787b6a34347d5da
Krishna-Ramchandani <89068822+Krishna-Ramchandani@users.noreply.github.com>
2021-08-31 23:26:37 +0530

    Add files via upload

---
+++ React/first-react-project/src/App.js
@@ -0,0 +1,29 @@
+//import './App.css';
+import React from "react";
+//import ExpenseItem from "./components/Expenses/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
+function App() {
+  const expenses = [
+    { id: "e1", title: "Thing 1", amount: "100", date: new Date(2021, 7, 1) },
+    { id: "e2", title: "Thing 2", amount: "200", date: new Date(2021, 8, 1) },
+    { id: "e3", title: "Thing 3", amount: "300", date: new Date(2021, 6, 1) },
+    { id: "e4", title: "Thing 4", amount: "400", date: new Date(2021, 4, 1) },
+  ];
+
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "First React Program"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  return (
+    <div className="App">
+      <h2>First React Program</h2>
+      <p>This is a paragraph</p>
+      <Expenses items={expenses} />
+    </div>
+  );
+}
+
+export default App;
