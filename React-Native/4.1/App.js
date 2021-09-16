55f7085509f9543f0b4ccf56639f8194a3da8bdc
Krishna-Ramchandani <89068822+Krishna-Ramchandani@users.noreply.github.com>
2021-09-16 15:31:16 +0530

    header, screen, color constant(theme),card,user ip
    header component, screen component, color theme constant component, card component, clean user input, controlling keyboard numeric input and dismiss keyboard, reset inputs, display values entered by user, showing alert message popup

---
+++ React-Native/4.1/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
+
+export default function App() {
+  return (
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
+      <StartGameScreen />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
