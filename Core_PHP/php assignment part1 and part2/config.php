8fa0b456ba966a6c28cf9fe584c510c8f19cfeef
Krishna-Ramchandani <89068822+Krishna-Ramchandani@users.noreply.github.com>
2021-09-27 23:21:11 +0530

    php project video assignment part 1 & 2(shareboard

---
+++ Core_PHP/php assignment part1 and part2/config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "shareboard");
+
+// Define URL
+define("ROOT_PATH", "http://localhost/bits_training/php assignment part1 and part2/");
+define("ROOT_URL", "http://localhost/bits_training/php assignment part1 and part2/");
