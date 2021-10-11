5a9649db3d5d2c84f24d8dfdf330af9f9c11391b
Krishna-Ramchandani <89068822+Krishna-Ramchandani@users.noreply.github.com>
2021-10-11 23:01:49 +0530

    Add files via upload

---
+++ yii2/ApiManager/ApiManager.php
@@ -0,0 +1,66 @@
+<?php
+namespace app\ApiManager;
+
+use app\models\Api;
+use app\ApiManager\ApiInterface;
+use Yii;
+use yii\web\NotFoundHttpException;
+
+class ApiManager implements ApiInterface{
+
+    public function create()
+    {
+        $model = new Api();
+        $params = Yii::$app->getRequest()->getBodyParams();
+        if(isset($params) && !empty($params)){
+            if($model->load($params) && $model->save()){
+                return $model;
+            }
+        } else{
+            $model->loadDefaultValues();
+        }
+        return $model;
+
+    }
+
+    public function edit($id)
+    {
+        $model = $this->findModel($id);
+        
+        $params = Yii::$app->getRequest()->getBodyParams();
+        if(isset($params) && !empty($params)){
+            if($model->load($params) && $model->save()){
+                return true;
+            }
+         }
+        return false;
+    }
+
+    
+    public function delete($id)
+    {
+        return $this->findModel($id)->delete();
+    }
+
+
+    public function view($id)
+    {
+        return $this->findModel($id);
+    }
+
+    public function index()
+    {
+        
+    }
+    protected function findModel($id)
+    {
+        if (($model = Api::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+
+}
+
+?>
